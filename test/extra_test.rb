7e4bf45288b27f64328948e46f2d60bb8229c670
yoshoku <yoshoku@outlook.com>
2025-09-13 15:16:43 +0900

    test: add test for fliplr instance method

--- test/extra_test.rb
+++ test/extra_test.rb
@@ -43,6 +43,15 @@ class NArrayExtraTest < NArrayTestBase
       assert_equal(na[0, 0], 5)
       assert_equal(na[1, 0], 5)
     end
+
+    test "#{dtype}#fliplr" do
+      a = dtype[[2, 3], [4, 5]]
+      na = a.fliplr
+      assert_equal(na[0, 0], 3)
+      assert_equal(na[0, 1], 2)
+      assert_equal(na[1, 0], 5)
+      assert_equal(na[1, 1], 4)
+    end
   end
 
   FLOAT_TYPES.each do |dtype|
