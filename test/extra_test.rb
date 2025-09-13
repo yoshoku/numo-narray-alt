6d43b935893941a7ac90bf9ad8a627703d036781
yoshoku <yoshoku@outlook.com>
2025-09-13 14:48:21 +0900

    test: add test for new_zeros instance method

--- test/extra_test.rb
+++ test/extra_test.rb
@@ -11,5 +11,15 @@ class NArrayExtraTest < NArrayTestBase
       assert_equal(na.shape[0], 2)
       assert_equal(na.shape[1], 3)
     end
+
+    test "#{dtype}#new_zeros" do
+      a = dtype[[1], [2]]
+      na = a.new_zeros
+      assert_equal(na.ndim, 2)
+      assert_equal(na.shape[0], 2)
+      assert_equal(na.shape[1], 1)
+      assert_equal(na[0, 0], 0)
+      assert_equal(na[1, 0], 0)
+    end
   end
 end
