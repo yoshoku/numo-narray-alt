259c6ba670f058d2c9163053c0eb33ecd2166457
yoshoku <yoshoku@outlook.com>
2025-11-03 10:51:08 +0900

    refactor: autofix with clang-format

--- ext/numo/narray/SFMT-params19937.h
+++ ext/numo/narray/SFMT-params19937.h
@@ -30,22 +30,14 @@
 #define ALTI_SR2_PERM64                                                                        \
   (vector unsigned char)(15, 0, 1, 2, 3, 4, 5, 6, 17, 8, 9, 10, 11, 12, 13, 14)
 #else /* For OTHER OSs(Linux?) */
-#define ALTI_SL1                                                                               \
-  { SL1, SL1, SL1, SL1 }
-#define ALTI_SR1                                                                               \
-  { SR1, SR1, SR1, SR1 }
-#define ALTI_MSK                                                                               \
-  { MSK1, MSK2, MSK3, MSK4 }
-#define ALTI_MSK64                                                                             \
-  { MSK2, MSK1, MSK4, MSK3 }
-#define ALTI_SL2_PERM                                                                          \
-  { 1, 2, 3, 23, 5, 6, 7, 0, 9, 10, 11, 4, 13, 14, 15, 8 }
-#define ALTI_SL2_PERM64                                                                        \
-  { 1, 2, 3, 4, 5, 6, 7, 31, 9, 10, 11, 12, 13, 14, 15, 0 }
-#define ALTI_SR2_PERM                                                                          \
-  { 7, 0, 1, 2, 11, 4, 5, 6, 15, 8, 9, 10, 17, 12, 13, 14 }
-#define ALTI_SR2_PERM64                                                                        \
-  { 15, 0, 1, 2, 3, 4, 5, 6, 17, 8, 9, 10, 11, 12, 13, 14 }
+#define ALTI_SL1 { SL1, SL1, SL1, SL1 }
+#define ALTI_SR1 { SR1, SR1, SR1, SR1 }
+#define ALTI_MSK { MSK1, MSK2, MSK3, MSK4 }
+#define ALTI_MSK64 { MSK2, MSK1, MSK4, MSK3 }
+#define ALTI_SL2_PERM { 1, 2, 3, 23, 5, 6, 7, 0, 9, 10, 11, 4, 13, 14, 15, 8 }
+#define ALTI_SL2_PERM64 { 1, 2, 3, 4, 5, 6, 7, 31, 9, 10, 11, 12, 13, 14, 15, 0 }
+#define ALTI_SR2_PERM { 7, 0, 1, 2, 11, 4, 5, 6, 15, 8, 9, 10, 17, 12, 13, 14 }
+#define ALTI_SR2_PERM64 { 15, 0, 1, 2, 3, 4, 5, 6, 17, 8, 9, 10, 11, 12, 13, 14 }
 #endif /* For OSX */
 #define IDSTR "SFMT-19937:122-18-1-11-1:dfffffef-ddfecb7f-bffaffff-bffffff6"
 
