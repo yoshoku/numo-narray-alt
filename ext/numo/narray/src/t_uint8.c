eb52e3af8fa063be6f4601106ad0dd907fb41986
yoshoku <yoshoku@outlook.com>
2025-12-22 08:44:50 +0900

    refactor: unify max_index method implementation across types with macro template

--- ext/numo/narray/src/t_uint8.c
+++ ext/numo/narray/src/t_uint8.c
@@ -48,6 +48,7 @@ extern VALUE cRT;
 #include "mh/min.h"
 #include "mh/max.h"
 #include "mh/ptp.h"
+#include "mh/max_index.h"
 #include "mh/maximum.h"
 #include "mh/minimum.h"
 #include "mh/minmax.h"
@@ -65,6 +66,7 @@ DEF_NARRAY_INT_PROD_METHOD_FUNC(uint8, numo_cUInt8, u_int64_t, numo_cUInt64)
 DEF_NARRAY_INT_MIN_METHOD_FUNC(uint8, numo_cUInt8)
 DEF_NARRAY_INT_MAX_METHOD_FUNC(uint8, numo_cUInt8)
 DEF_NARRAY_INT_PTP_METHOD_FUNC(uint8, numo_cUInt8)
+DEF_NARRAY_INT_MAX_INDEX_METHOD_FUNC(uint8)
 DEF_NARRAY_INT_MAXIMUM_METHOD_FUNC(uint8, numo_cUInt8)
 DEF_NARRAY_INT_MINIMUM_METHOD_FUNC(uint8, numo_cUInt8)
 DEF_NARRAY_INT_MINMAX_METHOD_FUNC(uint8, numo_cUInt8)
@@ -3414,89 +3416,6 @@ static VALUE uint8_clip(VALUE self, VALUE min, VALUE max) {
 }
 
 #define idx_t int64_t
-static void iter_uint8_max_index_index64(na_loop_t* const lp) {
-  size_t n, idx;
-  char *d_ptr, *i_ptr, *o_ptr;
-  ssize_t d_step, i_step;
-
-  INIT_COUNTER(lp, n);
-  INIT_PTR(lp, 0, d_ptr, d_step);
-
-  idx = f_max_index(n, d_ptr, d_step);
-
-  INIT_PTR(lp, 1, i_ptr, i_step);
-  o_ptr = NDL_PTR(lp, 2);
-  *(idx_t*)o_ptr = *(idx_t*)(i_ptr + i_step * idx);
-}
-#undef idx_t
-
-#define idx_t int32_t
-static void iter_uint8_max_index_index32(na_loop_t* const lp) {
-  size_t n, idx;
-  char *d_ptr, *i_ptr, *o_ptr;
-  ssize_t d_step, i_step;
-
-  INIT_COUNTER(lp, n);
-  INIT_PTR(lp, 0, d_ptr, d_step);
-
-  idx = f_max_index(n, d_ptr, d_step);
-
-  INIT_PTR(lp, 1, i_ptr, i_step);
-  o_ptr = NDL_PTR(lp, 2);
-  *(idx_t*)o_ptr = *(idx_t*)(i_ptr + i_step * idx);
-}
-#undef idx_t
-
-/*
-  Index of the maximum value.
-  @overload max_index(axis:nil)
-    @param [Numeric,Array,Range] axis  Finds maximum values along the axis and returns **flat
-    1-d indices**.
-    @return [Integer,Numo::Int] returns result indices.
-  @see #argmax
-  @see #max
-
-  @example
-      a = Numo::NArray[3,4,1,2]
-      a.max_index  #=> 1
-
-      b = Numo::NArray[[3,4,1],[2,0,5]]
-      b.max_index             #=> 5
-      b.max_index(axis:1)     #=> [1, 5]
-      b.max_index(axis:0)     #=> [0, 1, 5]
-      b[b.max_index(axis:0)]  #=> [3, 4, 5]
- */
-static VALUE uint8_max_index(int argc, VALUE* argv, VALUE self) {
-  narray_t* na;
-  VALUE idx, reduce;
-  ndfunc_arg_in_t ain[3] = { { Qnil, 0 }, { Qnil, 0 }, { sym_reduce, 0 } };
-  ndfunc_arg_out_t aout[1] = { { 0, 0, 0 } };
-  ndfunc_t ndf = { 0, STRIDE_LOOP_NIP | NDF_FLAT_REDUCE | NDF_EXTRACT, 3, 1, ain, aout };
-
-  GetNArray(self, na);
-  if (na->ndim == 0) {
-    return INT2FIX(0);
-  }
-  if (na->size > (~(u_int32_t)0)) {
-    aout[0].type = numo_cInt64;
-    idx = nary_new(numo_cInt64, na->ndim, na->shape);
-    ndf.func = iter_uint8_max_index_index64;
-
-    reduce = na_reduce_dimension(argc, argv, 1, &self, &ndf, 0);
-
-  } else {
-    aout[0].type = numo_cInt32;
-    idx = nary_new(numo_cInt32, na->ndim, na->shape);
-    ndf.func = iter_uint8_max_index_index32;
-
-    reduce = na_reduce_dimension(argc, argv, 1, &self, &ndf, 0);
-  }
-  rb_funcall(idx, rb_intern("seq"), 0);
-
-  return na_ndloop(&ndf, 3, self, idx, reduce);
-}
-
-#define idx_t int64_t
 static void iter_uint8_min_index_index64(na_loop_t* const lp) {
   size_t n, idx;
   char *d_ptr, *i_ptr, *o_ptr;
@@ -5061,6 +4980,25 @@ void Init_numo_uint8(void) {
    *   @return [Numo::UInt8] returns result of ptp.
    */
   rb_define_method(cT, "ptp", uint8_ptp, -1);
+  /**
+   * Index of the maximum value.
+   * @overload max_index(axis:nil)
+   *   @param [Numeric,Array,Range] axis  Finds maximum values along the axis and
+   *     returns **flat 1-d indices**.
+   *   @return [Integer,Numo::Int] returns result indices.
+   * @see #argmax
+   * @see #max
+   *
+   * @example
+   *     a = Numo::NArray[3,4,1,2]
+   *     a.max_index  #=> 1
+   *
+   *     b = Numo::NArray[[3,4,1],[2,0,5]]
+   *     b.max_index             #=> 5
+   *     b.max_index(axis:1)     #=> [1, 5]
+   *     b.max_index(axis:0)     #=> [0, 1, 5]
+   *     b[b.max_index(axis:0)]  #=> [3, 4, 5]
+   */
   rb_define_method(cT, "max_index", uint8_max_index, -1);
   rb_define_method(cT, "min_index", uint8_min_index, -1);
   rb_define_method(cT, "argmax", uint8_argmax, -1);
