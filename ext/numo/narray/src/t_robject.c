51916f94a41805b59410910ce2a3e1dc9498209f
yoshoku <yoshoku@outlook.com>
2026-01-04 19:15:54 +0900

    refactor: unify round method implementation across types with macro template

--- ext/numo/narray/src/t_robject.c
+++ ext/numo/narray/src/t_robject.c
@@ -61,6 +61,7 @@ VALUE cT;
 extern VALUE cRT;
 
 #include "mh/round/floor.h"
+#include "mh/round/round.h"
 #include "mh/clip.h"
 #include "mh/isnan.h"
 #include "mh/isinf.h"
@@ -94,6 +95,7 @@ extern VALUE cRT;
 typedef VALUE robject; // Type aliases for shorter notation
                        // following the codebase naming convention.
 DEF_NARRAY_ROBJ_FLOOR_METHOD_FUNC()
+DEF_NARRAY_ROBJ_ROUND_METHOD_FUNC()
 DEF_NARRAY_CLIP_METHOD_FUNC(robject, numo_cRObject)
 DEF_NARRAY_FLT_ISNAN_METHOD_FUNC(robject, numo_cRObject)
 DEF_NARRAY_FLT_ISINF_METHOD_FUNC(robject, numo_cRObject)
@@ -3089,64 +3091,6 @@ static VALUE robject_right_shift(VALUE self, VALUE other) {
   return robject_right_shift_self(self, other);
 }
 
-static void iter_robject_round(na_loop_t* const lp) {
-  size_t i, n;
-  char *p1, *p2;
-  ssize_t s1, s2;
-  size_t *idx1, *idx2;
-  dtype x;
-
-  INIT_COUNTER(lp, n);
-  INIT_PTR_IDX(lp, 0, p1, s1, idx1);
-  INIT_PTR_IDX(lp, 1, p2, s2, idx2);
-
-  if (idx1) {
-    if (idx2) {
-      for (i = 0; i < n; i++) {
-        GET_DATA_INDEX(p1, idx1, dtype, x);
-        x = m_round(x);
-        SET_DATA_INDEX(p2, idx2, dtype, x);
-      }
-    } else {
-      for (i = 0; i < n; i++) {
-        GET_DATA_INDEX(p1, idx1, dtype, x);
-        x = m_round(x);
-        SET_DATA_STRIDE(p2, s2, dtype, x);
-      }
-    }
-  } else {
-    if (idx2) {
-      for (i = 0; i < n; i++) {
-        GET_DATA_STRIDE(p1, s1, dtype, x);
-        x = m_round(x);
-        SET_DATA_INDEX(p2, idx2, dtype, x);
-      }
-    } else {
-      //
-      for (i = 0; i < n; i++) {
-        *(dtype*)p2 = m_round(*(dtype*)p1);
-        p1 += s1;
-        p2 += s2;
-      }
-      return;
-      //
-    }
-  }
-}
-
-/*
-  Unary round.
-  @overload round
-    @return [Numo::RObject] round of self.
-*/
-static VALUE robject_round(VALUE self) {
-  ndfunc_arg_in_t ain[1] = { { cT, 0 } };
-  ndfunc_arg_out_t aout[1] = { { cT, 0 } };
-  ndfunc_t ndf = { iter_robject_round, FULL_LOOP, 1, 1, ain, aout };
-
-  return na_ndloop(&ndf, 1, self);
-}
-
 static void iter_robject_ceil(na_loop_t* const lp) {
   size_t i, n;
   char *p1, *p2;
@@ -3601,6 +3545,11 @@ void Init_numo_robject(void) {
    *   @return [Numo::RObject] floor of self.
    */
   rb_define_method(cT, "floor", robject_floor, 0);
+  /**
+   * Unary round.
+   * @overload round
+   *   @return [Numo::RObject] round of self.
+   */
   rb_define_method(cT, "round", robject_round, 0);
   rb_define_method(cT, "ceil", robject_ceil, 0);
   rb_define_method(cT, "trunc", robject_trunc, 0);
--- ext/numo/narray/src/t_sfloat.c
+++ ext/numo/narray/src/t_sfloat.c
@@ -43,6 +43,7 @@ VALUE cT;
 extern VALUE cRT;
 
 #include "mh/round/floor.h"
+#include "mh/round/round.h"
 #include "mh/clip.h"
 #include "mh/isnan.h"
 #include "mh/isinf.h"
@@ -105,6 +106,7 @@ extern VALUE cRT;
 typedef float sfloat; // Type aliases for shorter notation
                       // following the codebase naming convention.
 DEF_NARRAY_FLT_FLOOR_METHOD_FUNC(sfloat, numo_cSFloat)
+DEF_NARRAY_FLT_ROUND_METHOD_FUNC(sfloat, numo_cSFloat)
 DEF_NARRAY_CLIP_METHOD_FUNC(sfloat, numo_cSFloat)
 DEF_NARRAY_FLT_ISNAN_METHOD_FUNC(sfloat, numo_cSFloat)
 DEF_NARRAY_FLT_ISINF_METHOD_FUNC(sfloat, numo_cSFloat)
@@ -3280,76 +3282,6 @@ static VALUE sfloat_nearly_eq(VALUE self, VALUE other) {
     v = rb_funcall(klass, id_cast, 1, self);
     return rb_funcall(v, id_nearly_eq, 1, other);
   }
-}
-
-static void iter_sfloat_round(na_loop_t* const lp) {
-  size_t i, n;
-  char *p1, *p2;
-  ssize_t s1, s2;
-  size_t *idx1, *idx2;
-  dtype x;
-
-  INIT_COUNTER(lp, n);
-  INIT_PTR_IDX(lp, 0, p1, s1, idx1);
-  INIT_PTR_IDX(lp, 1, p2, s2, idx2);
-
-  if (idx1) {
-    if (idx2) {
-      for (i = 0; i < n; i++) {
-        GET_DATA_INDEX(p1, idx1, dtype, x);
-        x = m_round(x);
-        SET_DATA_INDEX(p2, idx2, dtype, x);
-      }
-    } else {
-      for (i = 0; i < n; i++) {
-        GET_DATA_INDEX(p1, idx1, dtype, x);
-        x = m_round(x);
-        SET_DATA_STRIDE(p2, s2, dtype, x);
-      }
-    }
-  } else {
-    if (idx2) {
-      for (i = 0; i < n; i++) {
-        GET_DATA_STRIDE(p1, s1, dtype, x);
-        x = m_round(x);
-        SET_DATA_INDEX(p2, idx2, dtype, x);
-      }
-    } else {
-      //
-      if (is_aligned(p1, sizeof(dtype)) && is_aligned(p2, sizeof(dtype))) {
-        if (s1 == sizeof(dtype) && s2 == sizeof(dtype)) {
-          for (i = 0; i < n; i++) {
-            ((dtype*)p2)[i] = m_round(((dtype*)p1)[i]);
-          }
-          return;
-        }
-        if (is_aligned_step(s1, sizeof(dtype)) && is_aligned_step(s2, sizeof(dtype))) {
-          //
-          for (i = 0; i < n; i++) {
-            *(dtype*)p2 = m_round(*(dtype*)p1);
-            p1 += s1;
-            p2 += s2;
-          }
-          return;
-          //
-        }
-      }
-      for (i = 0; i < n; i++) {
-        GET_DATA_STRIDE(p1, s1, dtype, x);
-        x = m_round(x);
-        SET_DATA_STRIDE(p2, s2, dtype, x);
-      }
-      //
-    }
-  }
-}
-
-static VALUE sfloat_round(VALUE self) {
-  ndfunc_arg_in_t ain[1] = { { cT, 0 } };
-  ndfunc_arg_out_t aout[1] = { { cT, 0 } };
-  ndfunc_t ndf = { iter_sfloat_round, FULL_LOOP, 1, 1, ain, aout };
-
-  return na_ndloop(&ndf, 1, self);
 }
 
 static void iter_sfloat_ceil(na_loop_t* const lp) {
