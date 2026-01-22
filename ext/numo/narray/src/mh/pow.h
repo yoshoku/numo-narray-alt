c8d8bd11af510220b63c467d2f8c5cb6d7c604e0
yoshoku <yoshoku@outlook.com>
2026-01-22 20:57:44 +0900

    chore: add pow method macro template for xint64 arrays to resolve -Wshorten-64-to-32 warning

--- ext/numo/narray/src/mh/pow.h
+++ ext/numo/narray/src/mh/pow.h
@@ -67,6 +67,72 @@
     return rb_funcall(v, id_pow, 1, other);                                                    \
   }
 
+#define DEF_NARRAY_INT64_POW_METHOD_FUNC(tDType, tNAryClass)                                   \
+  static void iter_##tDType##_pow(na_loop_t* const lp) {                                       \
+    size_t n;                                                                                  \
+    char* p1;                                                                                  \
+    char* p2;                                                                                  \
+    char* p3;                                                                                  \
+    ssize_t s1;                                                                                \
+    ssize_t s2;                                                                                \
+    ssize_t s3;                                                                                \
+    INIT_COUNTER(lp, n);                                                                       \
+    INIT_PTR(lp, 0, p1, s1);                                                                   \
+    INIT_PTR(lp, 1, p2, s2);                                                                   \
+    INIT_PTR(lp, 2, p3, s3);                                                                   \
+    tDType x;                                                                                  \
+    tDType y;                                                                                  \
+    for (size_t i = 0; i < n; i++) {                                                           \
+      GET_DATA_STRIDE(p1, s1, tDType, x);                                                      \
+      GET_DATA_STRIDE(p2, s2, tDType, y);                                                      \
+      x = m_pow(x, (int)y);                                                                    \
+      SET_DATA_STRIDE(p3, s3, tDType, x);                                                      \
+    }                                                                                          \
+  }                                                                                            \
+                                                                                               \
+  static void iter_##tDType##_pow_int32(na_loop_t* const lp) {                                 \
+    size_t n;                                                                                  \
+    char* p1;                                                                                  \
+    char* p2;                                                                                  \
+    char* p3;                                                                                  \
+    ssize_t s1;                                                                                \
+    ssize_t s2;                                                                                \
+    ssize_t s3;                                                                                \
+    INIT_COUNTER(lp, n);                                                                       \
+    INIT_PTR(lp, 0, p1, s1);                                                                   \
+    INIT_PTR(lp, 1, p2, s2);                                                                   \
+    INIT_PTR(lp, 2, p3, s3);                                                                   \
+    tDType x;                                                                                  \
+    int32_t y;                                                                                 \
+    for (size_t i = 0; i < n; i++) {                                                           \
+      GET_DATA_STRIDE(p1, s1, tDType, x);                                                      \
+      GET_DATA_STRIDE(p2, s2, int32_t, y);                                                     \
+      x = m_pow_int(x, y);                                                                     \
+      SET_DATA_STRIDE(p3, s3, tDType, x);                                                      \
+    }                                                                                          \
+  }                                                                                            \
+                                                                                               \
+  static VALUE tDType##_pow_self(VALUE self, VALUE other) {                                    \
+    ndfunc_arg_in_t ain_i[2] = { { tNAryClass, 0 }, { numo_cInt32, 0 } };                      \
+    ndfunc_arg_in_t ain[2] = { { tNAryClass, 0 }, { tNAryClass, 0 } };                         \
+    ndfunc_arg_out_t aout[1] = { { tNAryClass, 0 } };                                          \
+    ndfunc_t ndf_i = { iter_##tDType##_pow_int32, STRIDE_LOOP, 2, 1, ain_i, aout };            \
+    ndfunc_t ndf = { iter_##tDType##_pow, STRIDE_LOOP, 2, 1, ain, aout };                      \
+    if (FIXNUM_P(other) || rb_obj_is_kind_of(other, numo_cInt32)) {                            \
+      return na_ndloop(&ndf_i, 2, self, other);                                                \
+    }                                                                                          \
+    return na_ndloop(&ndf, 2, self, other);                                                    \
+  }                                                                                            \
+                                                                                               \
+  static VALUE tDType##_pow(VALUE self, VALUE other) {                                         \
+    VALUE klass = na_upcast(rb_obj_class(self), rb_obj_class(other));                          \
+    if (klass == tNAryClass) {                                                                 \
+      return tDType##_pow_self(self, other);                                                   \
+    }                                                                                          \
+    VALUE v = rb_funcall(klass, id_cast, 1, self);                                             \
+    return rb_funcall(v, id_pow, 1, other);                                                    \
+  }
+
 #define DEF_NARRAY_ROBJ_POW_METHOD_FUNC()                                                      \
   static void iter_robject_pow(na_loop_t* const lp) {                                          \
     size_t n;                                                                                  \
