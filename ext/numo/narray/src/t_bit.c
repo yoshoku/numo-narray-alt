d51a4c40a5621ac527ecada2344d3866abc1bdb1
yoshoku <yoshoku@outlook.com>
2026-03-16 22:45:34 +0900

    revert: inline bit_each into t_bit.c and remove unused macro form each.h

--- ext/numo/narray/src/t_bit.c
+++ ext/numo/narray/src/t_bit.c
@@ -31,7 +31,6 @@ static ID id_to_a;
 VALUE cT;
 extern VALUE cRT;
 
-#include "mh/each.h"
 #include "mh/each_with_index.h"
 #include "mh/mean.h"
 #include "mh/var.h"
@@ -270,7 +269,40 @@ static VALUE bit_inspect(VALUE ary) {
   return na_ndloop_inspect(ary, iter_bit_inspect, Qnil);
 }
 
-DEF_NARRAY_BIT_EACH_METHOD_FUNC()
+static void iter_bit_each(na_loop_t* const lp) {
+  size_t n;
+  BIT_DIGIT* a1;
+  size_t p1;
+  ssize_t s1;
+  size_t* idx1;
+  INIT_COUNTER(lp, n);
+  INIT_PTR_BIT_IDX(lp, 0, a1, p1, s1, idx1);
+  BIT_DIGIT x = 0;
+  VALUE y;
+  if (idx1) {
+    for (size_t i = 0; i < n; i++) {
+      LOAD_BIT(a1, p1 + *idx1, x);
+      idx1++;
+      y = m_data_to_num(x);
+      rb_yield(y);
+    }
+  } else {
+    for (size_t i = 0; i < n; i++) {
+      LOAD_BIT(a1, p1, x);
+      p1 += s1;
+      y = m_data_to_num(x);
+      rb_yield(y);
+    }
+  }
+}
+
+static VALUE bit_each(VALUE self) {
+  ndfunc_arg_in_t ain[1] = { { Qnil, 0 } };
+  ndfunc_t ndf = { iter_bit_each, FULL_LOOP_NIP, 1, 0, ain, 0 };
+  na_ndloop(&ndf, 1, self);
+  return self;
+}
+
 DEF_NARRAY_BIT_EACH_WITH_INDEX_METHOD_FUNC()
 DEF_NARRAY_BIT_MEAN_METHOD_FUNC()
 DEF_NARRAY_BIT_VAR_METHOD_FUNC()
